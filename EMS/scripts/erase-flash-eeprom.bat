d57c471c802d5e19d8d5752a6ca4c5f4fa28895e
marco.accame <marco.accame@iit.it>
2015-09-30 12:26:50 +0200

    corrected bug
    the ul2 was not called even w/ -programmer UL2

--- EMS/scripts/erase-flash-eeprom.bat
+++ EMS/scripts/erase-flash-eeprom.bat
@@ -62,7 +62,7 @@ rem section for eraser
 mkdir reports
 copy ..\bin\environment\emsEEPROMerase.hex reports
 copy reports\emsEEPROMerase.hex tools\target.hex 
-C:\Keil_v5\UV4\UV4 -f tools\burn_target.uvprojx -o ..\reports\emsEEPROMerase_burn_report.txt -t ems4ulproeraseburn
+C:\Keil_v5\UV4\UV4 -f tools\burn_target.uvprojx -o ..\reports\emsEEPROMerase_burn_report.txt -t %target%
 del /F tools\*.bak
 del /F tools\*.dep
 del /F tools\*.plg
