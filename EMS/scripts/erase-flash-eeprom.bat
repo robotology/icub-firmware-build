c75b0bb27d8a8a8bf4541d74cc28f246471b68a5
marco.accame <marco.accame@iit.it>
2015-09-30 12:21:11 +0200

    added ulink2 programming support
    now the programming scripts are more evolute. they accept command line arguments:
    * command -help: prints help
    * command -programmer [UL2, ULPRO]: use a given programmer.
    * if used without argument the UL2 is used by default

--- EMS/scripts/erase-flash-eeprom.bat
+++ EMS/scripts/erase-flash-eeprom.bat
@@ -1,3 +1,64 @@
+
+@ECHO OFF
+ 
+IF "%1"=="" ( 
+    SET "Programmer=UL2" 
+    
+    GOTO TAGstart
+    
+    ) ELSE ( 
+        IF "%1"=="-programmer" (  
+            SET "Programmer=%2" 
+        ) ELSE IF "%1"=="-help" ( 
+            GOTO TAGhelp 
+        ) ELSE ( 
+            GOTO TAGsyntaxerror 
+        )
+
+        GOTO TAGstart
+) 
+
+
+
+:TAGsyntaxerror
+ECHO SYNTAX ERROR use "command -help" for command syntax 
+GOTO TAGend
+
+
+:TAGhelp
+ECHO USAGE
+ECHO command -help                  : prints this help
+ECHO command -programmer [ULPRO, UL2]   : uses UlinkPro (default) or Ulink2
+ECHO command                        : the same as -programmer ULPRO
+GOTO TAGend
+
+
+:TAGruntimeerror
+ECHO RUNTIME ERROR
+GOTO TAGend
+
+:TAGunrecognisedprogrammer
+ECHO ERROR: unrecognised programmer
+GOTO TAGend
+
+
+REM begin-work
+
+:TAGstart
+
+ECHO Programming flash-eeprom-eraser with %Programmer% (use command line option "-help" to see how to change programmer)
+
+IF %Programmer%==ULPRO (  
+            SET "target=ems4ulproeraseburn" 
+            ECHO Programming with %Programmer%
+            ECHO use option -programmer 
+        ) ELSE IF %Programmer%==UL2 ( 
+            SET "target=ems4ul2eraseburn" 
+        ) ELSE ( 
+            GOTO TAGunrecognisedprogrammer 
+        )
+
+rem section for eraser
 mkdir reports
 copy ..\bin\environment\emsEEPROMerase.hex reports
 copy reports\emsEEPROMerase.hex tools\target.hex 
@@ -8,3 +69,12 @@ del /F tools\*.plg
 del /F tools\*.uvgui*
 del /F tools\*.htm
 del /F tools\target.hex
+
+
+GOTO TAGend
+
+REM end-of-work
+
+:TAGend
+ECHO The script is over: BYE BYE
+
