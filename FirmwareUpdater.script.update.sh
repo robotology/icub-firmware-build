cfb61049c37d7d5905d3e815c12f2d5d43256d12
marcoaccame <marco.accame@iit.it>
2018-02-28 16:42:27 +0100

    just a placeholder for the automatic fw update of can and eth boards

---
+++ FirmwareUpdater.script.update.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+
+# this is a placeholder for a script which uses FirmwareUpdater
+# with command line options to update the firmware of ETH and 
+# CAN boards in the robot to the latest versions.
+
+echo ""
+echo "------------------------------------------------------"
+echo " the automatic fw updater script is under development "
+echo "------------------------------------------------------"
+echo ""
