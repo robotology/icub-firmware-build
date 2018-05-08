d72b344c207991fd1d1146902761f62ce5e78753
marcoaccame <marco.accame@iit.it>
2018-05-08 15:04:40 +0200

    sic... another attempt: using $YARP_ROBOT_NAME instead

--- scripts/FirmwareUpdater.script.update.left_leg.ems4.sh
+++ scripts/FirmwareUpdater.script.update.left_leg.ems4.sh
@@ -8,9 +8,9 @@ echo ""
 echo ""
 echo ""
 
-echo "this bash is executing: ./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOTNAME/network.$YARP_ROBOTNAME.xml -f ../info/firmware.info.xml -p left_leg -b ems4 -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOTNAME.update.left_leg.ems4.txt"
+echo "this bash is executing: ./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOT_NAME/network.$YARP_ROBOT_NAME.xml -f ../info/firmware.info.xml -p left_leg -b ems4 -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.left_leg.ems4.txt"
 echo ""
-./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOTNAME/network.$YARP_ROBOTNAME.xml -f ../info/firmware.info.xml -p left_leg -b ems4 -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOTNAME.update.left_leg.ems4.txt 
+./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOT_NAME/network.$YARP_ROBOT_NAME.xml -f ../info/firmware.info.xml -p left_leg -b ems4 -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.left_leg.ems4.txt 
 echo ""
-echo "this bash has executed: ./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOTNAME/network.$YARP_ROBOTNAME.xml -f ../info/firmware.info.xml -p left_leg -b ems4 -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOTNAME.update.left_leg.ems4.txt"
+echo "this bash has executed: ./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOT_NAME/network.$YARP_ROBOT_NAME.xml -f ../info/firmware.info.xml -p left_leg -b ems4 -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.left_leg.ems4.txt"
 echo ""
