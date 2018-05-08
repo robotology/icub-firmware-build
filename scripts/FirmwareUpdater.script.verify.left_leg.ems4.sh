584dc7765aeb74f267e5f6a059c610a754a44121
marcoaccame <marco.accame@iit.it>
2018-05-08 14:25:04 +0200

    added some more example scripts

---
+++ scripts/FirmwareUpdater.script.verify.left_leg.ems4.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+
+# this script which uses FirmwareUpdater by means of the python program manageFWrobot.py
+
+echo ""
+echo ""
+echo ""
+
+echo "this bash is executing: ./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$ICUB_ROBOTNAME/network.$ICUB_ROBOTNAME.xml -f ../info/firmware.info.xml -p left_leg -b ems4 -a verify | tee ../logs/log.of.FirmwareUpdater.$ICUB_ROBOTNAME.verify.left_leg.ems4.txt"
+echo ""
+./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$ICUB_ROBOTNAME/network.$ICUB_ROBOTNAME.xml -f ../info/firmware.info.xml -p left_leg -b ems4 -a verify | tee ../logs/log.of.FirmwareUpdater.$ICUB_ROBOTNAME.verify.left_leg.ems4.txt 
+echo ""
+echo "this bash has executed: ./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$ICUB_ROBOTNAME/network.$ICUB_ROBOTNAME.xml -f ../info/firmware.info.xml -p left_leg -b ems4 -a verify | tee ../logs/log.of.FirmwareUpdater.$ICUB_ROBOTNAME.verify.left_leg.ems4.txt"
+echo ""
