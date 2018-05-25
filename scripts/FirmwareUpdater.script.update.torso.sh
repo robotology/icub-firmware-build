6665d108ec514f2f14df69c6d687637224b3dfbb
marcoaccame <marco.accame@iit.it>
2018-05-25 13:25:13 +0200

    added more example scripts

---
+++ scripts/FirmwareUpdater.script.update.torso.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+
+# this script which uses FirmwareUpdater by means of the python program manageFWrobot.py
+
+echo ""
+echo ""
+echo ""
+
+echo "this bash is executing: ./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOT_NAME/network.$YARP_ROBOT_NAME.xml -f ../info/firmware.info.xml -p torso -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.torso.txt"
+echo ""
+./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOT_NAME/network.$YARP_ROBOT_NAME.xml -f ../info/firmware.info.xml -p torso -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.torso.txt 
+echo ""
+echo "this bash has executed: ./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOT_NAME/network.$YARP_ROBOT_NAME.xml -f ../info/firmware.info.xml -p torso -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.torso.txt"
+echo ""
