17a32a6ccec77806e5e063ebddc6c7892d04a108
tumme <davide.tome@iit.it>
2020-01-16 15:26:34 +0100

    added scripts to flash uP, using ST-LINK programmer and ST-LIN Utility, on following boards:
     - EMS
     - MC4PLUS
     - MTB4
     - STRAIN2

---
+++ ETH/EMS/scripts/st-link/st-link_EMS.bat
@@ -0,0 +1,31 @@
+:start
+@echo off
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
+if %OS%==32BIT set PATH=%ProgramFiles%
+if %OS%==64BIT set PATH=%ProgramFiles(x86)%
+
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -ME
+echo;
+echo;
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -c -P "..\bin\environment\emsLoader.hex"
+echo;
+echo;
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -c -P "..\bin\environment\emsUpdater.hex"
+echo;
+echo;
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -c -P "..\bin\application\ems.hex"
+echo;
+echo;
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -Rst -Run
+echo;
+echo;
+IF %errorlevel% NEQ 0 GOTO :error
+GOTO :end
+:error
+echo There was an error.
+EXIT 1
+:end
+echo End.
+EXIT 0
