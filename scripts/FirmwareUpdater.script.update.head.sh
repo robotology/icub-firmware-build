26c4f2964e904c2aa2d3121349574e31a3bc684a
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-02-01 21:44:33 +0100

    Generalize the FirmwareUpdater scripts to automatically find `network.$ROBOT_NAME.xml` file

--- scripts/FirmwareUpdater.script.update.head.sh
+++ scripts/FirmwareUpdater.script.update.head.sh
@@ -8,9 +8,9 @@ echo ""
 echo ""
 echo ""
 
-echo "this bash is executing: ./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOT_NAME/network.$YARP_ROBOT_NAME.xml -f ../info/firmware.info.xml -p head -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.head.txt"
+echo "this bash is executing: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p head -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.head.txt"
 echo ""
-./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOT_NAME/network.$YARP_ROBOT_NAME.xml -f ../info/firmware.info.xml -p head -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.head.txt 
+./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p head -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.head.txt 
 echo ""
-echo "this bash has executed: ./manageFWrobot.py -n $ROBOT_CODE/robots-configuration/$YARP_ROBOT_NAME/network.$YARP_ROBOT_NAME.xml -f ../info/firmware.info.xml -p head -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.head.txt"
+echo "this bash has executed: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p head -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.head.txt"
 echo ""
