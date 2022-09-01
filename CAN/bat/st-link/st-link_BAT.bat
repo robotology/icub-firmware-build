2b244a1c9e25e59826ca657087c44f5bbaaca7fc
Andrea Mura <andrea.mura@iit.it>
2022-09-01 13:19:37 +0200

    Update the BAT program procedure with the iCub3 and R1 targets (#59)

--- CAN/bat/st-link/st-link_BAT.bat
+++ CAN/bat/st-link/st-link_BAT.bat
@@ -6,17 +6,42 @@ reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" 
 if %OS%==32BIT set PATH=%ProgramFiles%
 if %OS%==64BIT set PATH=%ProgramFiles(x86)%
 
+echo;
+echo Select the target:
+echo [1] iCub3
+echo [2] R1
+set choice=
+set /p choice=Type the number: 
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto :iCub3
+if '%choice%'=='2' goto :R1
+echo "%choice%" is not valid, try again
+echo;
+goto :start
+:iCub3
 "%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -ME
 echo;
-echo;
-"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -c -P "..\bat.hex"
+echo Programming BAT with target iCub3
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -c -P "..\bat_1.0.0_icub3.hex"
 echo;
 echo;
 "%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -Rst -Run
 echo;
 echo;
-IF %errorlevel% NEQ 0 GOTO :error
-GOTO :end
+if  %errorlevel% NEQ 0 goto :error
+goto :end
+:R1
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -ME
+echo;
+echo Programming BAT with target R1
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -c -P "..\bat_1.0.0_r1.hex"
+echo;
+echo;
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -Rst -Run
+echo;
+echo;
+if %errorlevel% NEQ 0 goto :error
+goto :end
 :error
 echo There was an error.
 EXIT 1
