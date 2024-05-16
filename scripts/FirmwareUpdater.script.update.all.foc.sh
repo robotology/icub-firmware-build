c5db6b248d0cb50125a38e79f569698c4f2a2415
tumme <davide.tome@iit.it>
2024-05-16 16:03:54 +0200

    automatic firmware update - updated logs filenames for 2foc scripts

--- scripts/FirmwareUpdater.script.update.all.foc.sh
+++ scripts/FirmwareUpdater.script.update.all.foc.sh
@@ -8,9 +8,9 @@ echo ""
 echo ""
 echo ""
 
-echo "this bash is executing: ./manageFWrobot.py -n `yarp resource --from network.ergocub-2foc.xml` -f ../info/firmware.info.xml -p all -b foc -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.foc.txt"
+echo "this bash is executing: ./manageFWrobot.py -n `yarp resource --from network.ergocub-2foc.xml` -f ../info/firmware.info.xml -p all -b foc -a update | tee ../logs/log.of.FirmwareUpdater.ergocub-2foc..update.all.foc.txt"
 echo ""
-./manageFWrobot.py -n `yarp resource --from network.ergocub-2foc.xml | grep ^\".*$ | sed 's/"//g'` -f ../info/firmware.info.xml -p all -b foc -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.foc.txt 
+./manageFWrobot.py -n `yarp resource --from network.ergocub-2foc.xml | grep ^\".*$ | sed 's/"//g'` -f ../info/firmware.info.xml -p all -b foc -a update | tee ../logs/log.of.FirmwareUpdater..ergocub-2foc.update.all.foc.txt 
 echo ""
-echo "this bash has executed: ./manageFWrobot.py -n `yarp resource --from network.ergocub-2foc.xml` -f ../info/firmware.info.xml -p all -b foc -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.foc.txt"
+echo "this bash has executed: ./manageFWrobot.py -n `yarp resource --from network.ergocub-2foc.xml` -f ../info/firmware.info.xml -p all -b foc -a update | tee ../logs/log.of.FirmwareUpdater..ergocub-2foc.update.all.foc.txt"
 echo ""
