9b3f192e396374a53ea2ed574f67a85ef1aaf8a7
Davide Tomé <davide.tome@iit.it>
2024-05-22 10:34:05 +0200

    Update FirmwareUpdater.script.update.all.foc-special.sh

--- scripts/FirmwareUpdater.script.update.all.foc-special.sh
+++ scripts/FirmwareUpdater.script.update.all.foc-special.sh
@@ -7,6 +7,11 @@
 echo ""
 echo ""
 echo ""
+echo "this script is about to install ..... "
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+echo "If I'm here means the user wants to proceed"
+
 
 echo "this bash is executing: ./manageFWrobot.py -n `yarp resource --from network.ergocub-2foc-special.xml` -f ../info/firmware.info.xml -p all -b foc-special -a update | tee ../logs/log.of.FirmwareUpdater.ergocub-2foc-special.update.all.foc-special.txt"
 echo ""
