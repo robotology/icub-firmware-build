e1787bba7afcb675a2d8383ca13b02b9b32401b0
tumme <davide.tome@iit.it>
2024-05-22 11:59:49 +0200

    automatic FW update - updating scripts for 2foc update

--- scripts/FirmwareUpdater.script.update.all.foc-special.sh
+++ scripts/FirmwareUpdater.script.update.all.foc-special.sh
@@ -7,15 +7,15 @@
 echo ""
 echo ""
 echo ""
-echo "this script is about to install ..... "
+echo "This script is about to install the 2foc-special.hex FW binary with the HW FAULT on overheating disabled on all 2FOC boards of $YARP_ROBOT_NAME"
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
-echo "If I'm here means the user wants to proceed"
+echo "Starting the 2FOC update with 2foc-special.hex..."
 
 
-echo "this bash is executing: ./manageFWrobot.py -n `yarp resource --from network.ergocub-2foc-special.xml` -f ../info/firmware.info.xml -p all -b foc-special -a update | tee ../logs/log.of.FirmwareUpdater.ergocub-2foc-special.update.all.foc-special.txt"
+echo "this bash is executing: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.2foc-special.xml` -f ../info/firmware.info.xml -p all -b foc-special -a update | tee ../logs/log.of.FirmwareUpdater.ergocub-2foc-special.update.all.foc-special.txt"
 echo ""
-./manageFWrobot.py -n `yarp resource --from network.ergocub-2foc-special.xml | grep ^\".*$ | sed 's/"//g'` -f ../info/firmware.info.xml -p all -b foc-special -a update | tee ../logs/log.of.FirmwareUpdater.ergocub-2foc-special.update.all.foc-special.txt 
+./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.2foc-special.xml | grep ^\".*$ | sed 's/"//g'` -f ../info/firmware.info.xml -p all -b foc-special -a update | tee ../logs/log.of.FirmwareUpdater.ergocub-2foc-special.update.all.foc-special.txt 
 echo ""
-echo "this bash has executed: ./manageFWrobot.py -n `yarp resource --from network.ergocub-2foc-special.xml` -f ../info/firmware.info.xml -p all -b foc-special -a update | tee ../logs/log.of.FirmwareUpdater.ergocub-2foc-special.update.all.foc-special.txt"
+echo "this bash has executed: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.2foc-special.xml` -f ../info/firmware.info.xml -p all -b foc-special -a update | tee ../logs/log.of.FirmwareUpdater.ergocub-2foc-special.update.all.foc-special.txt"
 echo ""
