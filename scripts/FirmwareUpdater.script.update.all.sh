b51bc492b8dca64e4001235d1b88431f893fa005
Ines Sorrentino <43743081+isorrentino@users.noreply.github.com>
2024-07-24 14:22:15 +0200

    Fix bug in scripts for updating all the boards (#157)
    Co-authored-by: ergocub <ergocub@iit.it>

--- scripts/FirmwareUpdater.script.update.all.sh
+++ scripts/FirmwareUpdater.script.update.all.sh
@@ -7,10 +7,13 @@
 echo ""
 echo ""
 echo ""
+echo "This script is about to install the standard FW binary on all the boards of $YARP_ROBOT_NAME"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
 
 echo "this bash is executing: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p all -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.txt"
 echo ""
-./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p all -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.txt 
+./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml | grep ^\".*$ | sed 's/"//g'` -f ../info/firmware.info.xml -p all -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.txt
 echo ""
 echo "this bash has executed: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p all -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.txt"
 echo ""
