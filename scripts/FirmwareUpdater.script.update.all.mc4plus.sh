5ab28ab80f45c4bb8a59d0a0464da414ebd0ee6c
ergocub <ergocub@iit.it>
2024-07-24 10:50:03 +0200

    Fix bug in scripts for updating firmware

--- scripts/FirmwareUpdater.script.update.all.mc4plus.sh
+++ scripts/FirmwareUpdater.script.update.all.mc4plus.sh
@@ -10,7 +10,7 @@ echo ""
 
 echo "this bash is executing: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p all -b mc4plus -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.mc4plus.txt"
 echo ""
-./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p all -b mc4plus -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.mc4plus.txt
+./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml | grep ^\".*$ | sed 's/"//g'` -f ../info/firmware.info.xml -p all -b mc4plus -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.mc4plus.txt
 echo ""
 echo "this bash has executed: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p all -b mc4plus -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.all.mc4plus.txt"
 echo ""
