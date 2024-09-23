024b30d6d5db658550579b5d75961c73ff1d4a8a
Valentina Gaggero <valentina.gaggero@iit.it>
2024-09-23 12:13:13 +0200

    Updete all fw-updater script to parse the output on the yarp resource command correctly

--- scripts/FirmwareUpdater.script.update.torso.sh
+++ scripts/FirmwareUpdater.script.update.torso.sh
@@ -10,7 +10,7 @@ echo ""
 
 echo "this bash is executing: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p torso -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.torso.txt"
 echo ""
-./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p torso -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.torso.txt 
+./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml | grep ^\".*$ | sed 's/"//g'` -f ../info/firmware.info.xml -p torso -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.torso.txt 
 echo ""
 echo "this bash has executed: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p torso -a update | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.update.torso.txt"
 echo ""
--- scripts/FirmwareUpdater.script.verify.left_leg.ems4.sh
+++ scripts/FirmwareUpdater.script.verify.left_leg.ems4.sh
@@ -10,7 +10,7 @@ echo ""
 
 echo "this bash is executing: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p left_leg -b ems4 -a verify | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.verify.left_leg.ems4.txt"
 echo ""
-./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p left_leg -b ems4 -a verify | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.verify.left_leg.ems4.txt
+./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml | grep ^\".*$ | sed 's/"//g'` -f ../info/firmware.info.xml -p left_leg -b ems4 -a verify | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.verify.left_leg.ems4.txt
 echo ""
 echo "this bash has executed: ./manageFWrobot.py -n `yarp resource --from network.$YARP_ROBOT_NAME.xml` -f ../info/firmware.info.xml -p left_leg -b ems4 -a verify | tee ../logs/log.of.FirmwareUpdater.$YARP_ROBOT_NAME.verify.left_leg.ems4.txt"
 echo ""
